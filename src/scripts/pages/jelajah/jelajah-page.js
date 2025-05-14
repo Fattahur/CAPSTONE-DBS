84948a05a65d1b4665f76148b07635ea3a551662
rhmt2492 <fc129d5y1892@student.devacademy.id>
2025-05-15 00:25:25 +0700

    membuat tampilan Jelajah

--- src/scripts/pages/jelajah/jelajah-page.js
+++ src/scripts/pages/jelajah/jelajah-page.js
@@ -4,13 +4,54 @@
     const JelajahPage = {
       async render(container) {
         const template = html`
-          <section class="beranda">
-            
-            <div class="container">
-              <h1>Selamat Datang di Halaman Jelajah Budaya</h1>
-              <p>Ini adalah halaman utama setelah login berhasil.</p>
-            </div>
-          </section>
+        <main class="cerita-container">
+
+        <section class="top-bar">
+          <div class= "jdl-cerita-terpopuler">
+            Cerita Budaya</div>
+          <div class="search-filter-wrapper">
+            <input
+              type="text"
+              class="search-input"
+              placeholder="Cari cerita budaya..."
+            />
+            <select class="filter-select">
+              <option value="semua">Semua</option>
+              <option value="tarian">Tarian</option>
+              <option value="upacara">Upacara</option>
+            </select>
+          </div>
+        </section>
+
+        <!-- Rekomendasi Cerita -->
+        <section class="rekomendasi">
+
+          <div class="rekomendasi-grid">
+            ${[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(
+              () => html`
+                <div class="card-rekomendasi">
+                  <img class="img-square" src="images/dayak.jpg"  />
+                  
+                  <p class="judul-cerita">Cerita Budaya</p>
+
+                  <p class="desc">Deskripsi cerita budaya singkat...</p>
+                  <p class="lokasi">Indonesia</p>
+                  <div class="actions">
+                    <button class="selengkapnya">Selengkapnya</button>
+                    <div class="icon-group">
+                      <span>❤️</span>
+                      <span>💬</span>
+                      <span>🔖</span>
+                    </div>
+                  </div>
+                </div>
+              `
+            )}
+          </div>
+        </section>
+
+        </main>
+
         `;
         render(template, container);
       },
