4028c81e2caec9829baa2cde28992b8f7816fcdc
rhmt2492 <fc129d5y1892@student.devacademy.id>
2025-05-15 00:54:48 +0700

    memperbaiki webpack config untuk deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,41 @@
 
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   entry: './src/scripts/app.js', 
+//   output: {
+//     filename: 'main.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     clean: true,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',
+//     }),
+//   ],
+//   devServer: {
+//     static: {
+//       directory: path.resolve(__dirname, 'public'),
+//     },
+//     open: true,
+//   },
+  
+//   mode: 'development',
+// };
+
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/scripts/app.js', 
@@ -21,6 +56,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '.' }, // copy seluruh folder `public` ke root dist
+      ],
+    }),
   ],
   devServer: {
     static: {
@@ -28,6 +68,5 @@ module.exports = {
     },
     open: true,
   },
-  
   mode: 'development',
 };
