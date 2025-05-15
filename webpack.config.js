d63f8784822acb4d3426ead121735822a917ec1f
rhmt2492 <fc129d5y1892@student.devacademy.id>
2025-05-15 12:21:22 +0700

    perbaikan

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,6 @@
 
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//   entry: './src/scripts/app.js', 
-//   output: {
-//     filename: 'main.js',
-//     path: path.resolve(__dirname, 'dist'),
-//     clean: true,
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/i,
-//         use: ['style-loader', 'css-loader'],
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './src/index.html',
-//     }),
-//   ],
-//   devServer: {
-//     static: {
-//       directory: path.resolve(__dirname, 'public'),
-//     },
-//     open: true,
-//   },
-  
-//   mode: 'development',
-// };
-
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/scripts/app.js', 
@@ -56,11 +21,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public', to: '.' }, 
-      ],
-    }),
   ],
   devServer: {
     static: {
@@ -68,5 +28,45 @@ module.exports = {
     },
     open: true,
   },
+  
   mode: 'development',
 };
+
+
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// module.exports = {
+//   entry: './src/scripts/app.js', 
+//   output: {
+//     filename: 'main.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     clean: true,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',
+//     }),
+//     new CopyWebpackPlugin({
+//       patterns: [
+//         { from: 'public', to: '.' }, 
+//       ],
+//     }),
+//   ],
+//   devServer: {
+//     static: {
+//       directory: path.resolve(__dirname, 'public'),
+//     },
+//     open: true,
+//   },
+//   mode: 'development',
+// };
